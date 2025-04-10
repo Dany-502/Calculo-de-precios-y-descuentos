9f0d4ac44644b92c302727f7a5c389b9667f0f1e
MiguelAAH <miguelalbertoalonsoheredia@gmail.com>
2025-04-09 23:44:09 -0600

    Delete javadoc directory

--- javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"calcularprecio","l":"Conversor"},{"p":"calcularprecio","l":"Descuentos"},{"p":"calcularprecio","l":"FormatoPrecios"},{"p":"calcularprecio","l":"Impuestos"}];updateSearchResults();
