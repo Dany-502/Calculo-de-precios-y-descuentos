8c0f3ca5b9b9b3405b8ed0c5075de3e8de071a3e
MiguelAAH <miguelalbertoalonsoheredia@gmail.com>
2025-04-09 23:48:05 -0600

    Add files via upload

---
+++ docs/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"calcularprecio","l":"Conversor"},{"p":"calcularprecio","l":"Descuentos"},{"p":"calcularprecio","l":"FormatoPrecios"},{"p":"calcularprecio","l":"Impuestos"}];updateSearchResults();
